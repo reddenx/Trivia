df746f01e98a50cbd201cd1349868c67efc944c3
sm_d3po <seanmarek@gmail.com>
2020-07-31 15:18:16 -0700

    initial commit of all structures

---
+++ src/site/TriviaApi/AppStart/DependencyConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TriviaApi.AppStart
+{
+    public static class DependencyConfiguration
+    {
+    }
+}
